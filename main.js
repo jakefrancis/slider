61e7a814542aca5d8abb79d5dfd7e95e19618628
fluffmickgruff <jake.daniel.francis@gmail.com>
2021-02-24 22:05:16 -0500

    slide movement isolated into own function

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let cards = document.getElementById('viewport')
 
 
 
-let cardNumber = 4
+let cardNumber = 50
 
 let arrayOfNumbersFromNumber = (numberOfIndexes) =>{
 	let arr = []
@@ -49,7 +49,7 @@ for(let i = 0; i < cardNumber; i++){
 		default:
 			card.className='card'
 	}
-
+  card.textContent = `Slide:${i + 1}`
 	
 	cardWrapper.appendChild(card)
 	cards.appendChild(cardWrapper)
@@ -57,120 +57,102 @@ for(let i = 0; i < cardNumber; i++){
 
 
 const buildSlider = (id) => {
+  
   const slider = document.getElementById(String(id))
 
-  const sliderChildren = slider.children;
+  const sliderChildren = slider.children
+
+  //stores the intial value of a touch event or mouse click
+  let startX;
+
+  //the slider that is in view, the first index is one, the math was easier that way.
+  let childIndexInView = 1;
+
+  //Is the slider begin clicked or touched
+  let heldDown = false
+
+  //previous x position, establishes if the user is swiping right or left
+  let previous = 0;
+
+  //the x postition of the slides at the center of the slidewindow in relation to the translation of the x position
+  let posX = 0
+
+
+
+  const mouseDownListener = (event) => {
+	  event.preventDefault();
+    //record inital X position of the mouse
+	  startX = event.clientX
+    //mouse is being clicked	
+	  heldDown = true	
+  }
+
+  cards.addEventListener('mousedown', mouseDownListener)
 
   
-}
-
-
-
-
-
-let mainIndex = 1;
-
-
-let allElements = cards.children
-
-
-let heldDown = false
-let startX;
-let prev
-let currentX;
-let previous = 0;
-let posX = 0
-
-let timeDown;
-let timeUp;
-let difTime;
-
-let card = document.getElementById(mainIndex)
-
-
-const mouseDownListener = (event) => {
-	event.preventDefault();
-	startX = event.clientX
-	
-	heldDown = true
-
-	
-	
-}
-
-cards.addEventListener('mousedown', mouseDownListener)
-
-const mouseUpListener = (event) => {
-	event.preventDefault();
-	currentX = event.clientX
-	heldDown = false
   
-
-
-	resetPosition(posX)
-}
+  const mouseUpListener = (event) => {
+	  event.preventDefault();
+    //no longer clicking
+	  heldDown = false 
+    //resets slide to it's center at the center of the slide window
+	  resetPosition(posX)
+  }
 
 document.addEventListener('mouseup', mouseUpListener)
-
-const changeViewIndex = (direction, indexInView ,slideWidth,) => {
-
-}
-
-const moveSlides = (direction,distance,viewPosition) => {
-
- let changeX = viewDistance + distance
-
- if(direction === 'right'){
-  if (move === 'right'){
-	
-    if(posX > (-cardWidth * (mainIndex)) + ( cardWidth * 0.25)){
-      mainIndex--
-
-      if(mainIndex < 0){
-        mainIndex++
-        posX = (-cardWidth * (mainIndex)) + ( cardWidth * 0.25)
-      }
-      
-    }			
-  }
- }
-  
-}
 
 
 const mouseMoveListener = (event) => {
 	event.preventDefault();
-	previous = previous ? previous : startX 
+  //the distance traveled since last mouseMove event trigger
 	let distance = event.clientX - previous
-	let move = distance > 0 ? 'right': 'left'
+
+  //direction the mouse was moved
+
+  if(heldDown) {
+    moveSlide(distance)
+  }
 
 
-	if(heldDown) {
+
+	
+	previous = event.clientX;
+}
+
+cards.addEventListener('mousemove', mouseMoveListener)
+
+const moveSlide = (distance) => {
+
+  
+
+    let direction = distance > 0 ? 'right': 'left'
+    //needs to be own function
 		posX = posX + (distance)
 		
-		let cardWidth = allElements[0].offsetWidth
+		let cardWidth = sliderChildren[0].offsetWidth
 
-		if(move === 'left'){
+		if(direction === 'left'){
 			
-			if(posX < (-cardWidth * mainIndex) + (cardWidth * 0.75)){
-				mainIndex++
+			if(posX < (-cardWidth * childIndexInView) + (cardWidth * 0.75)){
+				childIndexInView++
 
 				
-				if(mainIndex > allElements.length){
-					mainIndex--
-					posX = (-cardWidth * mainIndex) + (cardWidth * 0.75)
+				if(childIndexInView > sliderChildren.length){
+					childIndexInView--
+					posX = (-cardWidth * childIndexInView) + (cardWidth * 0.75)
 				}
 				
 			}			
 		}
-		else if (move === 'right'){
+		else if (direction === 'right'){
 	
-			if(posX > (-cardWidth * (mainIndex -1)) + ( cardWidth * 0.25)){
-				mainIndex--
+			if(posX > (-cardWidth * (childIndexInView - 1)) + ( cardWidth * 0.25)){
+  
+				childIndexInView--
 
-				if(mainIndex < 1){
-					mainIndex++
-					posX = (-cardWidth * (mainIndex -1)) + ( cardWidth * 0.25)
+				if(childIndexInView < 1){
+					childIndexInView++
+					posX = (-cardWidth * (childIndexInView - 1)) + ( cardWidth * 0.25)
 				}
 				
 			}			
@@ -178,8 +160,8 @@ const mouseMoveListener = (event) => {
 		
 			
 		
-		for(let i = 0; i < allElements.length; i++){
-			let card = allElements[i]			
+		for(let i = 0; i < sliderChildren.length; i++){
+			let card = sliderChildren[i]			
 		
 			
 			
@@ -191,11 +173,8 @@ const mouseMoveListener = (event) => {
 		
 		
 	}
-	
-	previous = event.clientX;
-}
 
-cards.addEventListener('mousemove', mouseMoveListener)
+
 
 
 
@@ -206,20 +185,20 @@ const resetPosition = (currentPosition) => {
 
   let endPos = currentPosition
 	const k = 0.1
-	const resetPos = (mainIndex - 1) * allElements[0].offsetWidth
+	const resetPos = (childIndexInView - 1) * sliderChildren[0].offsetWidth
 	let dX = k  * (resetPos + posX)
 	
-	var move = function() {
+	var direction = function() {
 		
 		endPos -= dX
 		dX = k * (resetPos + endPos)
-		for(let i = 0; i < allElements.length; i++){
-			let card = allElements[i]
+		for(let i = 0; i < sliderChildren.length; i++){
+			let card = sliderChildren[i]
 		card.style.transform = `translateX(${endPos}px)`
 		
 		if(endPos < -resetPos + 2 && endPos >  -resetPos - 2){
-			for(let i = 0; i < allElements.length; i++){
-				let card = allElements[i]
+			for(let i = 0; i < sliderChildren.length; i++){
+				let card = sliderChildren[i]
 			card.style.transform = `translateX(${-resetPos}px)`
 			}
 			posX = -resetPos
@@ -228,17 +207,17 @@ const resetPosition = (currentPosition) => {
 		}
 		
 		
-		window.requestAnimationFrame(move)
+		window.requestAnimationFrame(direction)
 }
-	window.requestAnimationFrame(move)
+	window.requestAnimationFrame(direction)
 	
 }
 
 const centerViewport = () => {
-  posX = -allElements[0].offsetWidth * (mainIndex - 1
+  posX = -sliderChildren[0].offsetWidth * (childIndexInView - 1
 )
-  for(let i = 0; i < allElements.length; i++){
-    let card = allElements[i] 
+  for(let i = 0; i < sliderChildren.length; i++){
+    let card = sliderChildren[i] 
     
     
     card.style.transform = `translateX(${posX}px)`
@@ -249,3 +228,13 @@ const centerViewport = () => {
 
 
 window.onresize = centerViewport
+
+  
+}
+
+
+buildSlider('viewport')
+
+
+
+
